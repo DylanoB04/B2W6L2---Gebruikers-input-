7b2e25246b1d4f0b4a105ed90dc88cd2bb572a0d
Dylano Brouwer <99064035@mydavinci.nl>
2021-01-12 14:43:06 +0100

    commit 12/1/2021
    labje af

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-var container = document.getElementById("container");
-var amount = 0;
-var names = [];
 
-while(amount < 3) {
-    amount = prompt("Hoeveel namen wilt u toevoegen? (Minimaal 3.)");
-    list1();
+
+
+var nameList=[];
+var amountOf=0;
+var text=document.getElementById('text');
+var text2=document.getElementById('text2');
+
+
+var amount=prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3).");
+if(!amount<3){
+    for(i=0;i<amount;i++){
+        nameList.push(prompt("Voer een naam in."));
+    };
+    for(i=0;i<nameList.length;i++){
+        text.innerHTML=nameList;
+    };
+    for(i=0;i<nameList.length;i++){
+        text2.innerHTML=nameList.reverse();
+    };
 };
 
-function list1() {
-       for(i = 0; i < amount; i ++) {
-        var nameInput = prompt("Voer een naam in.");
-        names.push(nameInput);
-        var h2 = document.createElement("h2");
-        container.appendChild(h2);
-        var node = document.createTextNode(names[i]);
-        h2.appendChild(node);
-    }; 
-};
+
+
+// "woord in array zetten, elk letter van woordje in array, als ingevoerde letter niet op juiste plaats staat dan gaat door array  heen kijken, iets break"
+
+// ".value"
+
+
+// var btn = document.getElementById('btn');
+// var names = ["dylano", "pietje", "janhenk", "achmet"];
+// var names2 = document.getElementById('txtInput');
+
+// btn.onclick = searchName;
+
+// function searchName() {
+//     var person = names2.value;
+//     var place = names.indexOf(person);
+//     document.write
+// };
+
+
+
+
+
+
+
+
+
